6567c4cc66810347d9cb279c919afe334b534584
Guilherme Alves <g.policarto@gmail.com>
2023-10-06 21:44:07 -0300

    Ajustes de texto no conteúdo;

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function calcularIdade() {
+
+    const dataAtual = new Date();
+
+    const anoAtual = dataAtual.getFullYear();
+    const mesAtual = dataAtual.getMonth() + 1; 
+
+    const dataNasc = new Date(1996, 9, 21);
+
+    const anoNasc = dataNasc.getFullYear();
+    const mesNasc = dataNasc.getMonth() + 1;
+
+    let idade = anoAtual - anoNasc;
+
+    if (mesAtual < mesNasc) {
+        idade--;
+    }
+
+    document.getElementById("idade").innerText = idade;
+}
+
+
+calcularIdade();
